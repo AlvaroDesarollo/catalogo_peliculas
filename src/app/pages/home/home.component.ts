83f34e4b2b150036556fc6897acce6de95c64f82
Alvaro Torres <alvarojavier.torres@dxc.com>
2023-11-30 23:43:40 -0500

    feat(): implementacion de carrusel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICarrusel } from 'src/app/models/carrusel.model';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public titulo: string= '';
+  public titulo: string = '';
+  public imagenesCarrusel: ICarrusel[] = [];
 
-  constructor(
-  ) {}
+  constructor() {}
 
   ngOnInit() {
-    this.titulo = 'Home'
+    this.titulo = 'Home';
+    this.setImagenesCarrusel();
+  }
+
+  setImagenesCarrusel() {
+    const carrusel: ICarrusel[] = [
+      {
+        detalleImage: 'imagen1',
+        url: 'https://media.istockphoto.com/id/1416933841/photo/dune-landscape.jpg?s=1024x1024&w=is&k=20&c=r2qnb3siuZVR99e6s3t7qYro6IZp9T9p8L1JXSWTqn4=',
+        id: '1',
+      },
+      {
+        detalleImage: 'imagen2',
+        url: 'https://media.istockphoto.com/id/1416933841/photo/dune-landscape.jpg?s=1024x1024&w=is&k=20&c=r2qnb3siuZVR99e6s3t7qYro6IZp9T9p8L1JXSWTqn4=',
+        id: '2',
+      },
+      {
+        detalleImage: 'imagen3',
+        url: 'https://media.istockphoto.com/id/1416933841/photo/dune-landscape.jpg?s=1024x1024&w=is&k=20&c=r2qnb3siuZVR99e6s3t7qYro6IZp9T9p8L1JXSWTqn4=',
+        id: '3',
+      },
+      {
+        detalleImage: 'imagen4',
+        url: 'https://media.istockphoto.com/id/1416933841/photo/dune-landscape.jpg?s=1024x1024&w=is&k=20&c=r2qnb3siuZVR99e6s3t7qYro6IZp9T9p8L1JXSWTqn4=',
+        id: '4',
+      },
+      {
+        detalleImage: 'imagen5',
+        url: 'https://media.istockphoto.com/id/1416933841/photo/dune-landscape.jpg?s=1024x1024&w=is&k=20&c=r2qnb3siuZVR99e6s3t7qYro6IZp9T9p8L1JXSWTqn4=',
+        id: '5',
+      },
+      {
+        detalleImage: 'imagen6',
+        url: 'https://media.istockphoto.com/id/1416933841/photo/dune-landscape.jpg?s=1024x1024&w=is&k=20&c=r2qnb3siuZVR99e6s3t7qYro6IZp9T9p8L1JXSWTqn4=',
+        id: '6',
+      },
+    ];
+
+    this.imagenesCarrusel = carrusel;
+  }
+
+  detallePelicula(id: string){
+    console.log('detallePelicula', id);
+
   }
 }
